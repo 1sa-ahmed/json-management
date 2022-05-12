e86b4523cd2d2e7ff19b432118dd4366ef35ea3d
1sa_ahmed <adahmedt@gmail.com>
2022-05-11 20:02:30 -0400

    json learned

---
+++ readme.js
@@ -0,0 +1,21 @@
+// MERN 5-8-22 http://www.youtube.com/watch?v=9Z6chXJs9D8&ab
+// MERN 5-8-22 https://www.youtube.com/watch?v=PN6r96PxGO8&ab
+
+/** JSON - Javascript object notation
+ * it is a data format
+ * it helps to convert array/object to an string
+ * it will mostly used for api
+ * json itself isn't a server but can be treat as server
+ * You can't make a comment in json normally
+ */
+
+// const devs = [
+//   {
+//     id: 1,
+//   },
+// ];
+
+// JSON
+// Students name, class, roll
+
+// Teacher name, age, skill nite paren.
